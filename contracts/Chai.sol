fcbfa36a54d75c20259d188a1090faa71fd2be78
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-04 00:47:14 +0530

    update

--- contracts/Chai.sol
+++ contracts/Chai.sol
@@ -1,20 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity^0.8.12;
 
 contract chai{
-    struct Memo{
-        string name;
-        string message;
-        uint256 timestamp;
-       address from;
-    }
-
-    Memo[] memos;
-    address payable owner;
-
-    constructor() {
-        owner=payable(msg.sender);
-    }  
-
-    function buyChai(string )
+       
 }
